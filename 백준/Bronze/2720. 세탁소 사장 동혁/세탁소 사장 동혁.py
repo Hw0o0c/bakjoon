02ddd224eb5ff216c689e97dfcfe7377b770dc39
Hw0o0c <128034210+Hw0o0c@users.noreply.github.com>
2023-08-15 20:11:27 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 172 ms, Memory: 114844 KB -BaekjoonHub

---
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -0,0 +1,18 @@
+case = int(input())
+coin = []
+def change(base) :
+    if base <= 500 :
+        print((int(base / 25)), end=' ')
+        if (base % 25) < 25 :
+            print((int((base % 25) / 10)), end=' ')
+            if ((base % 25) % 10) < 10 :
+                print((int((base % 25) % 10 / 5)), end=' ')
+                if ((base % 25) % 10 % 5) < 5 :
+                    print((int((base % 25) % 10 % 5)))
+    return coin
+
+
+
+for i in range(case) :
+    money = int(input())
+    change(money)
